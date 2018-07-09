370404046b2cd2f9cc5c62f077ea1f4382bcd1fe
Jim Geurts <jim@biacreations.com>
2018-07-09 13:34:47 -0500

    End the test after all assertions

--- test/client.test.js
+++ test/client.test.js
@@ -1265,6 +1265,7 @@ test('unbind (GH-483) - Call unbind callback when socket is connected', function
     setImmediate(function () {
       t.ok(true, 'Checking unbind callback status');
       t.ok(callbackCalled, 'should have called unbind callback');
+      t.end();
     });
   });
 });
