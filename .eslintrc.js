9ac1daaea53d826b79ab8322831bf7201e6ba621
Tony Brix <tony@brix.ninja>
2020-12-05 17:52:52 -0600

    chore(lint): use eslint instead of standard

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  rules: {
+    'no-shadow': 'warn',
+    'no-unused-vars': 'warn'
+  }
+}
