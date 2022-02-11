02077a277931640d14a5e68781cff328c84d2f4e
Daniel Fiterman <fiterman@amazon.com>
2022-02-10 19:52:48 -0500

    force socket to be closed on destroy

--- test/client.test.js
+++ test/client.test.js
@@ -1747,3 +1747,24 @@ tap.only('emitError', function (t) {
 
   t.end()
 })
+
+tap.test('socket destroy', function (t) {
+  const clt = ldap.createClient({
+    socketPath: t.context.socketPath,
+    bindDN: BIND_DN,
+    bindCredentials: BIND_PW
+  })
+
+  clt.once('connect', function () {
+    t.ok(clt)
+    clt._socket.once('close', function () {
+      t.ok(!clt.connected)
+      t.end()
+    })
+    clt.destroy()
+  })
+
+  clt.once('destroy', function () {
+    t.ok(clt.destroyed)
+  })
+})
