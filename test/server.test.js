188870d7b5e53b513f62b7c86d92344e99767cd2
Max Leiter <mleiter@usc.edu>
2022-06-07 17:59:33 -0700

    Progress on supporting IPv6 (#805)
    * Progress on supporting IPv6
    
    * Apply suggestions from code review
    
    Co-authored-by: James Sumners <james@sumners.email>
    
    * tests: add IPv6 URL format test to server.test.js
    
    Co-authored-by: James Sumners <james@sumners.email>

--- test/server.test.js
+++ test/server.test.js
@@ -24,7 +24,7 @@ tap.test('basic create', function (t) {
 tap.test('connection count', function (t) {
   const server = ldap.createServer()
   t.ok(server)
-  server.listen(0, 'localhost', function () {
+  server.listen(0, '127.0.0.1', function () {
     t.ok(true, 'server listening on ' + server.url)
 
     server.getConnections(function (err, count) {
@@ -57,6 +57,17 @@ tap.test('properties', function (t) {
   // listen on a random port so we have a url
   server.listen(0, 'localhost', function () {
     t.ok(server.url)
+
+    server.close(() => t.end())
+  })
+})
+
+tap.test('IPv6 URL is formatted correctly', function (t) {
+  const server = ldap.createServer()
+  t.equal(server.url, null, 'url empty before bind')
+  server.listen(0, '::1', function () {
+    t.ok(server.url)
+    t.equal(server.url, 'ldap://[::1]:' + server.port)
 
     server.close(() => t.end())
   })
@@ -437,7 +448,7 @@ tap.test('multithreading support via external server', function (t) {
     config: serverOptions
   }
   t.ok(server)
-  fauxServer.listen(5555, 'localhost', function () {
+  fauxServer.listen(5555, '127.0.0.1', function () {
     t.ok(true, 'server listening on ' + server.url)
 
     t.ok(fauxServer)
@@ -459,7 +470,7 @@ tap.test('multithreading support via hook', function (t) {
   const server = ldap.createServer(serverOptions)
   const fauxServer = ldap.createServer(serverOptions)
   t.ok(server)
-  fauxServer.listen(0, 'localhost', function () {
+  fauxServer.listen(0, '127.0.0.1', function () {
     t.ok(true, 'server listening on ' + server.url)
 
     t.ok(fauxServer)
