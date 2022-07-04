d8221b18573a251a2045460c5730134793a6b00d
Sascha Tandel <514405+sastan@users.noreply.github.com>
2022-07-04 13:35:56 +0200

    test: add strict mode test for sub-class value getter

--- test/controls/control.test.js
+++ test/controls/control.test.js
@@ -51,3 +51,37 @@ test('parse no value', function (t) {
   t.notOk(c.value, null)
   t.end()
 })
+
+test('only value getter in sub-class with strict mode', function (t) {
+  "use strict"
+
+  function SubControl (options) {
+    assert.optionalObject(options)
+    options = options || {}
+    options.type = SubControl.OID
+    if (options.value) {
+      this._value = options.value
+      options.value = null
+    }
+    Control.call(this, options)
+  }
+  util.inherits(SubControl, Control)
+  Object.defineProperties(SubControl.prototype, {
+    value: {
+      get: function () { return this._value || {} },
+      configurable: false
+    }
+  })
+
+  SubControl.OID = '2.16.840.1.113730.3.4.2'
+  
+  const c = new SubControl({
+    value: { size: 3 },
+    criticality: true
+  })
+  t.ok(c)
+  t.equal(c.type, '2.16.840.1.113730.3.4.2')
+  t.ok(c.criticality)
+  t.equal(c.value.size, 3)
+  t.end()
+})
