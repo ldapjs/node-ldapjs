fae14e3aeaf6b1e346db7b4ccdd2cfefbf40f553
James Sumners <james@sumners.email>
2023-02-21 13:28:58 -0500

    Replace messages with @ldapjs/messages

--- test/messages/parser.test.js
+++ test/messages/parser.test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { test } = require('tap')
-const { Parser, LDAPMessage, LDAP_REQ_EXTENSION } = require('../../lib')
+const { Parser } = require('../../lib')
 
 test('wrong protocol error', function (t) {
   const p = new Parser()
@@ -13,37 +13,4 @@ test('wrong protocol error', function (t) {
 
   // Send some bogus data to incur an error
   p.write(Buffer.from([16, 1, 4]))
-})
-
-test('bad protocol op', function (t) {
-  const p = new Parser()
-  const message = new LDAPMessage({
-    protocolOp: 254 // bogus (at least today)
-  })
-  p.once('error', function (err) {
-    t.ok(err)
-    t.ok(/not supported$/.test(err.message))
-    t.end()
-  })
-  p.write(message.toBer())
-})
-
-test('bad message structure', function (t) {
-  const p = new Parser()
-
-  // message with bogus structure
-  const message = new LDAPMessage({
-    protocolOp: LDAP_REQ_EXTENSION
-  })
-  message._toBer = function (writer) {
-    writer.writeBuffer(Buffer.from([16, 1, 4]), 80)
-    return writer
-  }
-
-  p.once('error', function (err) {
-    t.ok(err)
-    t.end()
-  })
-
-  p.write(message.toBer())
 })
